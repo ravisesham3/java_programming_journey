595cd93fb2327d82de3405b2edf7c33a81978071
ravisesham3 <ravindrasesham4@gmail.com>
2025-09-17 20:38:19 +0530

    Instance variable

---
+++ Instance.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Instance {
+    public static void main(String[] args) {
+        byte age =22;               //In primitive there is no object creation
+        Date now = new Date();      //where as in Non-primitive/Instance variable create object cls
+        System.out.println(now);            //for ex--Date now = new Date();
+    }                                                   //now.time();
+}
