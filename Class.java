9955873f0400190eee0991ded891d305d9417ce6
ravisesham3 <ravindrasesham4@gmail.com>
2025-09-17 20:00:18 +0530

    variable

---
+++ Class.java
@@ -0,0 +1,6 @@
+public class Class {
+    public static void main(String[] args){
+        int age = 25;                      //int integer value, age is a variable,which is used to store the value.
+        System.out.println(age);
+    }
+}
