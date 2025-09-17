85f94f71ec33fdaa376d1f6ea574865611b850d8
ravisesham3 <ravindrasesham4@gmail.com>
2025-09-17 20:24:40 +0530

    primitive

---
+++ Primitive.java
@@ -0,0 +1,10 @@
+public class Primitive {
+    public static void main(String[] args) {
+        byte age = 30;
+        long viewcounts = 123_456_789L; //To add L in last position because it identifies as long same as all
+        float price = 25.99F;
+        char letters = 'A';         //In this single character using single quorts(''), where multi char or strings are using double quorts("")
+        boolean isEligible = true;
+        System.out.println(viewcounts);
+    }
+}
