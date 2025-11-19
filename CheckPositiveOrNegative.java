44853eb5b17f78d06e6338aee67e0f08a63c670f
ravisesham3 <ravindrasesham4@gmail.com>
2025-11-19 23:33:08 +0530

    check positive number or negative number

---
+++ CheckPositiveOrNegative.java
@@ -0,0 +1,11 @@
+public class CheckPositiveOrNegative {
+    public static void main(String[] args) {
+        int num=-5;
+        if(num>0)
+            System.out.println("value is positive");
+        else
+            System.out.println("Value is negative");
+    }
+}
+//output:
+// value is negative
