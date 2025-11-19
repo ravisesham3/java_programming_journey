b0acbf0f332f6e43b3cf60749643a3b959773558
ravisesham3 <ravindrasesham4@gmail.com>
2025-11-19 23:04:16 +0530

    Check Even OR Odd

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num=12;
+        if(num%2==0)
+            System.out.println("Even number");
+        else
+            System.out.println("Odd number");
+    }
+    
+}
+//output:
+//Even number
---
+++ Object.java
@@ -0,0 +1,10 @@
+import javafx.scene.effect.Light.Point;
+
+public class Object {
+    public static void main(String[] args) {
+        Point point1 = new Point(x:1, y:1);
+        Point point2 = point1;
+        point1.x= 2;
+        System.out.println(point2);
+    }
+}
