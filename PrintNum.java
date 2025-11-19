c9e4fdf1785df041372f1fdb2c64d00f66ad3fbb
ravisesham3 <ravindrasesham4@gmail.com>
2025-11-19 23:14:04 +0530

    Print 1 To 10 numbers using for loop

---
+++ PrintNum.java
@@ -0,0 +1,8 @@
+public class PrintNum {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++)
+        System.out.println(i);
+    }
+}
+//output:
+//1 2 3 4 5 6 7 8 9 10 column wise
