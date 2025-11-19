942b7df534b492ce9c9c88763176d80dd753a672
ravisesham3 <ravindrasesham4@gmail.com>
2025-11-19 23:10:18 +0530

    Find Larger value

---
+++ LargeNum.java
@@ -0,0 +1,11 @@
+public class LargeNum {
+    public static void main(String[] args) {
+        int a=20,b=2;
+        if(a>b)
+            System.out.println(a+ "is larger");
+        else
+            System.out.println(b+ "is larger");
+    }
+}
+//output:
+//20 is larger
