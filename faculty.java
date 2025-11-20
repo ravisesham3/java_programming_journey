06a97e4594d3b3ae29a57d278e6934a0e4a7452d
ravisesham3 <ravindrasesham4@gmail.com>
2025-11-20 11:43:25 +0530

    Inherite properties from parent to child

---
+++ faculty.java
@@ -0,0 +1,16 @@
+class Student {
+    void display(){
+        System.out.println("I am a Student");
+    }
+}
+public class faculty {
+
+    
+} {
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.display();
+    }
+
+}
+
